22edcda2a08cb85263ca4aa1413a0b65a36a2c18
赵慧杰 <zhaohuijie@jd.com>
2018-05-28 10:33:50 +0800

    第一次提交

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const uglify = require('uglifyjs-webpack-plugin');
+const htmlPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    //入口
+    entry: {
+       entry: './src/entry.js',
+       entry2: './src/entry2.js'
+    },
+    //出口
+    output: {
+        path: path.resolve(__dirname,'./dist'),//使用./dist和dist一样
+        filename: '[name].js'
+    },
+    module: {  
+        rules:[
+            {
+                test: /\.css$/,
+                use: ['style-loader','css-loader']
+            },{
+                test: /\.(png|jpg|gif)/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 500000
+                    }
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new uglify(),
+        new htmlPlugin({
+            minify: {
+                removeAttributeQuotes: true
+            },
+            hash: true,
+            template: './src/index.html'
+        })
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname,'./dist'),
+        host: 'lm.jd.com',
+        compress: true,
+        port: 3023
+    }
+}
